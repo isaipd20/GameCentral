33c8ed0c8c88c30d72566d589f640b30edd4a8d3
Thezerolok <isaipd20@gmail.com>
2024-03-31 23:08:21 -0700

    Testing javascript

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 let d = new Date();
 alert("Today's date is " + d);
+
+var button = document.getElementById("testbutton");
+button.addEventListener("click", changeColor);
+
+function changeColor() {
+    document.getElementsByClassName("test").style.backgroundColor = "green";
+  }
