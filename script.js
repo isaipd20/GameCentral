44a08964b6510ada74e8b70e96f449723442297d
Thezerolok <isaipd20@gmail.com>
2024-04-27 23:36:26 -0700

    Testing javascript

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function changeColor() {
   const button = document.getElementById("myButton");
   button.style.backgroundColor = "red";
 }
+
+function changeC() {
+  const button = document.getElementById("myButton");
+  button.style.backgroundColor = "blue";
+}
