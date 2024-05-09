b79553d53a0d1abd08e3aef3af3950d1e4e76e4f
Thezerolok <isaipd20@gmail.com>
2024-05-08 19:15:59 -0700

    Timer added

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ function changeC() {
   const button = document.getElementById("myButton");
   button.style.backgroundColor = "green";
 }
+
+function timer(){
+  var sec = 30;
+  var timer = setInterval(function(){
+      document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
+      sec--;
+      if (sec < 0) {
+          clearInterval(timer);
+      }
+  }, 1000);
+}
